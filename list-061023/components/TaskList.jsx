ba7886c7871197564e4afdc7734919168cd77973
Paula Cano <paulacanoroman@gmail.com>
2023-10-06 21:39:11 +0200

    funcionalidad básica

--- list-061023/components/TaskList.jsx
+++ list-061023/components/TaskList.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TaskList = () => {
+
+    const [dataList, setDataList] = useState([]);
+
+    const [dataInput, setDataInput] = useState("");
+
+
   return (
-    <div>TaskList</div>
+    <>
+    <input onChange={(e)=>{setDataInput(e.target.value)}} value={dataInput}></input>
+    <button onClick={()=>{setDataList(dataInput)}}>enviar lista</button>
+    {dataList}
+    </>
   )
 }
 
